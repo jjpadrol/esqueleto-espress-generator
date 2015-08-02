0b8ea9cab426ee39ac20a5372feff3dcc737c382
jjpadrol <jjpadrol@gmail.com>
2015-08-02 18:01:11 +0200

    creación de pregutnas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -47,6 +47,23 @@ exports.answer = function (req, res) {
 
 };
 
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( //crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"});
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+
+	//guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+ 		res.redirect('/quizes');
+	}) //Redirección HTTP (Url relativo) lista de preguntas
+};
+
 
 
 
