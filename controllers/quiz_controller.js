3b74eec9afa0d361f2c4647448c631c1b6ec7d9f
jjpadrol <jjpadrol@gmail.com>
2015-08-02 19:53:56 +0200

    borrar pregutnas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -103,6 +103,16 @@ exports.update = function (req, res) {
 	);
 };
 
+// DELETE /quizes/:id
+exports.destroy = function (req, res) {
+	req.quiz
+	.destroy()
+	.then(
+		 function() { res.redirect('/quizes');}
+		 )
+		.catch(function(error){next(error)});
+};
+
 
 // Author
 exports.author = function(req, res) {
